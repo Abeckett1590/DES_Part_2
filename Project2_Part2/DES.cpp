6b3fb3bf37a0abeece20706a167b7c508777a88e
tannerr05 <tannerpersonalemail@gmail.com>
2026-02-22 18:00:03 -0600

    Implemented decryption function

--- Project2_Part2/DES.cpp
+++ Project2_Part2/DES.cpp
@@ -77,6 +77,63 @@ string encryption(string plaintext, vector<string> keys) { //takes in 64 bit pla
     return ciphertext;
 }
 
-string decryption(string encrypted, vector<string> keys) {
+string decryption(string encrypted, vector<string> keys) { //takes in 64 bit ciphertext and 16 48-bit keys, returns 64 bit plaintext
+    string newEncrypted = "";
+    for(int i=0; i<64; i++){ //apply initial permutation
+        newEncrypted+=encrypted[IP[i]-1];
+    }
     
+    string L = newEncrypted.substr(0,32); //split into left and right halves (32 bits each)
+    string R = newEncrypted.substr(32,32);
+
+    for(int r=15;r>=0;r--){ //perform 16 rounds with keys used in reverse order for decryption
+        string key = keys[r];
+
+        string lastL = L; //old L is saved for XOR later
+        L=R; //new L becomes previous R (Feistel swap)
+
+        string expanded;
+        for(int i=0;i<48;i++){ //expand R to 48-bits with expansion table
+            expanded+=R[E[i]-1];
+        }
+
+        string xor1 = "";
+        for(int i=0;i<48;i++){ //xor expanded R with current round's key
+            xor1+=char(((expanded[i]-'0')^(key[i]-'0'))+'0');
+        }
+
+        string sboxed = "";
+
+        for(int i=0; i<8; i++){ //divide R into eight 6-bit blocks for use with SBOXMAP
+                string sixbits = xor1.substr(i*6,6);
+
+                int row=(sixbits[0]-'0') * 2 + (sixbits[5]-'0');
+                int col=(sixbits[1]-'0') * 8 +
+                        (sixbits[2]-'0') * 4 +
+                        (sixbits[3]-'0') * 2 +
+                        (sixbits[4]-'0');
+
+                int sboxval=SBOXMAP[i][row*16+col];
+
+                bitset<4> bits(sboxval);
+                sboxed+=bits.to_string();
+        }
+
+        string perm = "";
+        for(int i=0; i<32; i++){
+            perm+=sboxed[P[i]-1];
+        }
+
+        for(int i=0; i<32; i++){
+            R[i] = char(((perm[i]-'0')^(lastL[i]-'0'))+'0');
+        }
+    }
+
+    string temp = R+L; //swap halves
+
+    string plaintext = "";
+    for(int i=0; i<64; i++){ //apply final permutation
+        plaintext+=temp[FP[i]-1];
+    }
+    return plaintext;
 }
